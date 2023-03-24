c9d1a79c4c4d09409eca03d0a9ea359a6a1745fe
gorety <wagutugorety@gmail.com>
2023-03-24 13:51:57 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+function  writeCards() {
+    const response = []
+    let g;
+    for (g = 0; g<gifts.length; g++){
+        response[g]=`Thank you, ${gifts[g]}, for the wonderful surprise gift!`
+    }
+    return response;
+}
+function countDown() {
+        let g = 10;
+        while (g>=0)
+         console.log(g--);
+ }
+countDown()
